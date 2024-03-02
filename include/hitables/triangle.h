bf95a58050e56dbfcce10dfb65b6b3d1017987ad
RobbeDGreef <robbedegreef@gmail.com>
2024-03-03 00:55:28 +0100

    fix: correct initialization technique for list types

--- include/hitables/triangle.h
+++ include/hitables/triangle.h
@@ -14,7 +14,7 @@ protected:
 
 public:
     Triangle(Point3 x, Point3 y, Point3 z, std::shared_ptr<Material> mat) 
-             : m_points({x,y,z}), m_mat(mat) {}
+             : m_points{x,y,z}, m_mat(mat) {}
 
     bool hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const override;
     bool boundingBox(double t0, double t1, AABB &bounding_box) const override;
