5eaf4dc277702632532746a71d732e34f31c04e4
RobbeDGreef <robbedegreef@gmail.com>
2024-04-10 13:13:34 +0200

    refactor: added schlickFresnes function to material

--- include/materials/material.h
+++ include/materials/material.h
@@ -6,6 +6,9 @@
 
 class Material
 {
+protected:
+    double schlickFresnel(double u);
+
 public: 
     virtual bool scatter(const Ray &r, const HitRecord &rec, Color &attenuation, Ray &scattered) const = 0;
     virtual Color emitted(double u, double v, const Point3 &p) const { return Color(0,0,0); }
