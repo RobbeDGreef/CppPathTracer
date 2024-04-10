288382676c9a2524baf5668cd07e0259a700a6d9
RobbeDGreef <robbedegreef@gmail.com>
2024-04-10 13:14:53 +0200

    refactor: created convience functions for testing raytracer

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 #include <hitables/hitable_list.h>
 #include <hitables/sphere.h>
-#include <hitables/moving_sphere.h>
 #include <hitables/triangle.h>
 
 #include <materials/lambertian.h>
@@ -16,8 +15,10 @@
 #include <textures/noise_texture.h>
 #include <textures/image_texture.h>
 
-#include <obj.h>
+#include <fileformats/obj.h>
+#include <fileformats/gltf.h>
 
+#if 0
 HitableList genRandom()
 {
     HitableList world;
@@ -25,7 +26,7 @@ HitableList genRandom()
     auto mat_ground = std::make_shared<Lambertian>(std::make_shared<CheckerTexture>());
     world.add(std::make_shared<Sphere>(Point3(0,-1000, 0), 1000, mat_ground));
 
-    # if 1
+#if 1
     for (int a = -11; a < 11; a++)
     {
         for (int b = -11; b < 11; b++)
@@ -56,7 +57,7 @@ HitableList genRandom()
             }
         }
     }
-    #endif
+#endif
 
     auto mat1 = std::make_shared<Dielectric>(1.5);
     world.add(std::make_shared<Sphere>(Point3(0,1,0), 1.0, mat1));
@@ -66,18 +67,44 @@ HitableList genRandom()
     world.add(std::make_shared<Sphere>(Point3(4,1,0), 1.0, mat3));
 
     return world;
-    
 }
+#endif
 
-int main(int argc, char **argv)
+Renderer createBenchmarkScene()
 {
     const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    DEBUG("Rendering image of size: " << image_width << "x" << image_height);
-    
-    
+    Obj object = Obj("objects/suzanne.obj");
+    Scene scene = object.read();
+
+    Camera cam = Camera(Point3(5, 2, 3), Point3(0, 0, 0), aspect_ratio, 60, 0.001, -1, 0, 1);    
+    scene.setCamera(cam);
+
+    Renderer renderer(image_width, image_height, scene, Color(0.5, 0.55, 0.9));
+    renderer.render(150);
+    renderer.writeToFile("test.bmp");
+    return renderer;
+}
+
+Renderer renderFastBenchmarkScene()
+{
+    const double aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    GLTF gltf = GLTF("benchmarking/test.glb");
+    Scene scene = gltf.read();
+
+    Renderer renderer(image_width, image_height, scene, Color(0.5, 0.55, 0.9));
+    renderer.render(150);
+    renderer.writeToFile("test.bmp");
+    return renderer;
+}
+
+int main(int argc, char **argv)
+{
 #if 0
     HitableList world = genRandom();
 #endif
@@ -107,7 +134,7 @@ int main(int argc, char **argv)
     auto doge = std::make_shared<ImageTexture>("textures/earthmap.bmp");
     //auto doge = std::make_shared<SolidColor>(Color(1,0,0));
     world.add(std::make_shared<Sphere>(Point3(0, 0, 0), 1, std::make_shared<Lambertian>(doge)));
-    world.add(std::make_shared<Sphere>(Point3(0, -101, 0), 100, std::make_shared<Lambertian>(doge)));
+    world.add(std::make_semptyhared<Sphere>(Point3(0, -101, 0), 100, std::make_shared<Lambertian>(doge)));
 #endif
 
 #if 0
@@ -123,26 +150,15 @@ int main(int argc, char **argv)
 #if 0
     HitableList world;
     auto mat = std::make_shared<Lambertian>(Color(1,0,0));
-    world.add(std::make_shared<Triangle>(Point3(0,0,0), Point3(0.5,1,1), Point3(0.2,0,1), mat));    
+    world.add(std::make_shared<Triangle>(Point3(0,0,0), Point3(0.5,1,1), Point3(0.2,0,1), mat));
 #endif
 
 #if 0
     HitableList world;
     auto mat = std::make_shared<Lambertian>(Color(1,0,0));
-    world.add(std::make_shared<Triangle>(Point3(0,0,0), Point3(1,1,0), Point3(0.5,0,0), mat));    
+    world.add(std::make_shared<Triangle>(Point3(0,0,0), Point3(1,1,0), Point3(0.5,0,0), mat));
 #endif
 
-#if 1
-    Obj object = Obj("objects/cube.obj");
-    HitableList world = object.read();
-    //world.add(std::make_shared<Sphere>(Point3(0,-101,0), 100, std::make_shared<Lambertian>(Color(0.5,0.5,0.5))));
-    //world.add(std::make_shared<Triangle>(Point3(-1, 0, 1+0.25),Point3(1, 0, 1+0.25),Point3(1, 1, 1+0.25), normshade));
-#endif
-
-    Camera cam(Point3(5,2,3), Point3(0,0,0), aspect_ratio, 60, 0.001, -1, 0, 1);
-    Renderer renderer(image_width, image_height, world, cam, Color(0.5, 0.55, 0.9));
-    renderer.render(300);
-    renderer.writeToFile("test.bmp");
- 
+    renderFastBenchmarkScene();
     return 0;
 }
