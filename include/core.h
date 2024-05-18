e004d7b14d93ceb3cd2f463baf3351cc380fe106
RobbeDGreef <robbedegreef@gmail.com>
2024-05-18 19:16:02 +0200

    refactor: moved ERROR and WARN to core

--- include/core.h
+++ include/core.h
@@ -24,6 +24,18 @@
 #define PACKED( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop))
 #endif
 
+#define __TERM_YELLOW      "\u001b[33;1m"
+#define __TERM_RED         "\u001b[31;1m"
+#define __TERM_PURPLE      "\u001b[35;1m"
+#define __TERM_COLOR_END   "\u001b[0m"
+
+
+#define __DW(x) do { io_mutex.lock(); x; io_mutex.unlock(); } while (0)
+#define COLOR(color, x) color << x << __TERM_COLOR_END
+
+#define WARN(x) __DW(std::cerr << COLOR(__TERM_PURPLE, "[WARNING] ") << x << '\n')
+#define ERROR(x) __DW(std::cerr << COLOR(__TERM_RED, "[ERROR] ") << x << '\n')
+
 #define OUT(x) std::cout << x << "\n";
 
 const double inf = std::numeric_limits<double>::infinity();
--- include/debug.h
+++ include/debug.h
@@ -3,17 +3,8 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
-
-#define YELLOW      "\u001b[33;1m"
-#define RED         "\u001b[31;1m"
-#define PURPLE      "\u001b[35;1m"
-#define COLOR_END   "\u001b[0m"
+#include <core.h>
 
 extern std::mutex io_mutex;
 
-#define DW(x) do { io_mutex.lock(); x; io_mutex.unlock(); } while (0)
-#define COLOR(color, x) color << x << COLOR_END
-
-#define DEBUG(x) DW(std::cout << COLOR(YELLOW, "[DEBUG] ") << x << '\n')
-#define WARN(x) DW(std::cout << COLOR(PURPLE, "[WARNING] ") << x << '\n')
-#define ERROR(x) DW(std::cout << COLOR(RED, "[ERROR] ") << x << '\n')
+#define DEBUG(x) __DW(std::cerr << COLOR(__TERM_YELLOW, "[DEBUG] ") << x << '\n')
